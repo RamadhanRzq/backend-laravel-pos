f11e1a2c1e1bb5e0dd29f7bab5707338de3030f3
naufaldzakiy <dzakiynaufal206@gmail.com>
2026-06-29 21:10:46 +0700

    add prefix auth

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -5,13 +5,16 @@ use App\Http\Controllers\Api\MapUsersToRoleController;
 use App\Http\Controllers\Api\RoleController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/me', [AuthController::class, 'me']);
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
 
-    Route::apiResource('roles', RoleController::class);
-    Route::apiResource('mapuserstorole', MapUsersToRoleController::class);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'me']);
+
+        Route::apiResource('roles', RoleController::class);
+        Route::apiResource('mapuserstorole', MapUsersToRoleController::class);
+    });
 });
